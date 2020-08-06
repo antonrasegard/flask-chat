db49bf2cd7c218d6c579832f7f360ae85d914706
antonrasegard <arasegard@gmail.com>
2020-08-06 19:01:21 +0000

    A little bit of mardi gras in my life. And a bit of python code

--- run.py
+++ run.py
@@ -13,4 +13,9 @@ def index():
     return "To send a message use /USERNAME/MESSAGE"
 
 
+@app.route('/<username>')
+def user(username):
+    return "Hi " + username
+
+
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
