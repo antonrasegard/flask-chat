6714fb8951702fa5d04e429f9bc20da94e13b56c
antonrasegard <arasegard@gmail.com>
2020-08-06 14:12:44 +0000

    Changed message for index page

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
 import os
 
+
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "<h1>Hello There!</h1>"
+    """Main page with instructions"""
+    return "To send a message use /USERNAME/MESSAGE"
 
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
